7e7733b407235e551a4bc0eabe65f5ef910393c2
Ahmed <darwish144.ad@gmail.com>
2021-12-26 23:27:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+class CountTimer{
+    constructor(durationInput,startButton,pauseButton){
+        this.durationInput = durationInput;
+        this.startButton   = startButton;
+        this.pauseButton   = pauseButton;
+
+        this.startButton.addEventListener('click',this.start);
+
+
+
+    }
+start() {
+    console.log(this)
+}
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+
+const countTimer = new CountTimer(durationInput,startButton,pauseButton);
+// countTimer.start();
+
+// Explanation
+// console.log(this);
+
+// const printThis = () => {
+//     console.log(this);
+// }
+
+// const ahmed = function () {
+//         console.log(this);    
+// };
+
+// ahmed();
+
+const colors = {
+    printColor(){
+        console.log(this)
+
+    }
+
+};
+
+const randomObjct = {
+    c:4
+}
+
+randomObjct.printColor = colors.printColor;
+randomObjct.printColor();
